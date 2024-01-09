35dadba8243f0fa50f533eb930d431dcc5c9376e
ldmfabio <ldmfabio@gmail.com>
2024-01-09 16:41:07 -0300

    Aula 26: Working with dash_table

---
+++ 6-dash_datatable.py
@@ -0,0 +1,20 @@
+from dash import Dash, dcc, html, dash_table
+import pandas as pd
+
+app = Dash()
+
+df = pd.read_csv(
+    "https://raw.githubusercontent.com/plotly/datasets/master/gapminder2007.csv"
+)
+# print(df)
+
+app.layout = html.Div([
+    html.Div(children='DataTable com Dash'),
+    dash_table.DataTable(
+        data=df.to_dict('records'),
+        page_size=10,
+    )
+])
+
+if __name__ == '__main__':
+    app.run_server(debug=True)

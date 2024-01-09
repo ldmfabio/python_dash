773828e49c7e0dfa854a98da454dece563d33d2d
ldmfabio <ldmfabio@gmail.com>
2024-01-09 15:42:18 -0300

    Aula 19

--- dataset.py
+++ dataset.py
@@ -1,7 +1,8 @@
 import pandas as pd
 
 ecom_sales = pd.read_csv('dados/ecom_sales.csv')
-ecom_sales = ecom_sales.groupby('Country')['OrderValue'].agg('sum').reset_index(name='Total Sales (R$)')
+ecom_sales_novo = ecom_sales.groupby('Country')['OrderValue'].agg('sum').reset_index(name='Total Sales (R$)')
 # print(ecom_sales.head())
 # print(ecom_sales.tail())
-print(ecom_sales)
+# print(ecom_sales)
+# print(ecom_sales_novo)
